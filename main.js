a46530760b373ed166f9afb7d6ea7bde54473622
Pontus Persson <pontus.alexander@gmail.com>
2017-10-17 15:22:35 +0200

    Import boilerplate.

--- main.js
+++ main.js
@@ -1,29 +1,5 @@
-function createShader(gl, type, source) {
-  const shader = gl.createShader(type);
-  gl.shaderSource(shader, source);
-  gl.compileShader(shader);
-  const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
-  if (success) {
-    return shader;
-  }
-
-  console.log(gl.getShaderInfoLog(shader));
-  gl.deleteShader(shader);
-}
-
-function createProgram(gl, vertexShader, fragmentShader) {
-  const program = gl.createProgram();
-  gl.attachShader(program, vertexShader);
-  gl.attachShader(program, fragmentShader);
-  gl.linkProgram(program);
-  const success = gl.getProgramParameter(program, gl.LINK_STATUS);
-  if (success) {
-    return program;
-  }
-
-  console.log(gl.getProgramInfoLog(program));
-  gl.deleteProgram(program);
-}
+import {compileShader} from './shader.js';
+import {createProgram} from './program.js';
 
 function loadShader(name) {
   return fetch(`./shaders/${name}.glsl`).then(r => r.text());
@@ -39,8 +15,8 @@ function main() {
     loadShader('fragment-shader'),
   ])
   .then(([vertexShaderSource, fragmentShaderSource]) => {
-    const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
-    const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+    const vertexShader = compileShader(gl, vertexShaderSource, gl.VERTEX_SHADER);
+    const fragmentShader = compileShader(gl, fragmentShaderSource, gl.FRAGMENT_SHADER);
     const program = createProgram(gl, vertexShader, fragmentShader);
     console.log(vertexShader, fragmentShader, program);
 
